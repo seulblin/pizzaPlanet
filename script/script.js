73c8d3b8683a7c1a2642d43b8ef7f8528ae3fe77
seulblin <subinge94@gmail.com>
2020-12-21 17:21:26 +0900

    new

---
+++ script/script.js
@@ -0,0 +1,21 @@
+let doc = document.documentElement;
+let scrolls;
+let containerOne = document.getElementById('burger');
+let bgTxtOne = document.getElementById('thisis');
+let bgTxtTwo = document.getElementById('analien');
+let bgTxtThr = document.getElementById('burgersub');
+
+let stuff = document.querySelectorAll('meterial');
+let sectionOne = [];
+
+
+window.addEventListener('scroll',function(){
+
+ scrolls = doc.scrollTop;
+ console.log(scrolls);
+
+ if(scrolls > 150){
+    containerOne.classList.add('opacitya');
+ }
+
+});
