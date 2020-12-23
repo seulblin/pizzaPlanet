90b6549ff2c9d0a2dff06f174f41da15d2c96ffa
seulblin <subinge94@gmail.com>
2020-12-23 14:42:00 +0900

    add javascript

--- script/script.js
+++ script/script.js
@@ -1,21 +1,30 @@
 let doc = document.documentElement;
 let scrolls;
-let containerOne = document.getElementById('burger');
-let bgTxtOne = document.getElementById('thisis');
-let bgTxtTwo = document.getElementById('analien');
-let bgTxtThr = document.getElementById('burgersub');
-
-let stuff = document.querySelectorAll('meterial');
-let sectionOne = [];
-
+let one = document.getElementById('burTop');
+let menuTwo = document.getElementById('burCu');
 
 window.addEventListener('scroll',function(){
-
- scrolls = doc.scrollTop;
- console.log(scrolls);
-
- if(scrolls > 150){
-   // containerOne.classList.add('opacitya');
- }
-
+  scrolls = doc.scrollTop;
+  console.log(scrolls);
+  
+  if(scrolls > 150 && scrolls < 200){
+    //console.log('빵');
+    one.style.top = 2 + 'vh';
+  }
+  if(scrolls > 200 && scrolls < 250){
+    one.style.top = 4 + 'vh';
+  }
+  if(scrolls > 250 && scrolls < 300){
+    one.style.top = 6 + 'vh';
+  }
+  if(scrolls > 300 && scrolls < 350){
+    one.style.top = 8 + 'vh';
+    menuTwo.style.top = 8 + 'vh';
+  }
+  if(scrolls > 350 && scrolls < 400){
+    one.style.top = 10 + 'vh';
+  }
+  if(scrolls > 400 && scrolls < 450){
+    one.style.top = 12 + 'vh';
+  }
 });
